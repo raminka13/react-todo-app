2e7e3af9d83a9bc40d0c8ff2b4e92728079dd41f
Raminka <raminka13@hotmail.com>
2022-08-08 14:14:08 -0500

    Add README

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './functionBased/components/Navbar';
+import TodoContainer from './functionBased/components/TodoContainer';
+import About from './functionBased/pages/About';
+import NotMatch from './functionBased/pages/NotMatch';
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<TodoContainer />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<NotMatch />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
